bb6785f7ca83b67b2e6c0d099127e3e2bda43947
lokizero00 <lokizero0@live.com>
2019-03-06 11:20:23 +0800

    修改配置

--- sass-service-zone/src/main/java/com/loki/sass/service/zone/api/DoorService.java
+++ sass-service-zone/src/main/java/com/loki/sass/service/zone/api/DoorService.java
@@ -1,15 +1,8 @@
 package com.loki.sass.service.zone.api;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 /**
  * created by lokizero00 on 2019-03-05
  */
-@Slf4j
-@Service
-@Transactional
 public interface DoorService {
 
 }
--- sass-service-zone/src/main/java/com/loki/sass/service/zone/service/DoorServiceImpl.java
+++ sass-service-zone/src/main/java/com/loki/sass/service/zone/service/DoorServiceImpl.java
@@ -1,9 +1,15 @@
 package com.loki.sass.service.zone.service;
 
 import com.loki.sass.service.zone.api.DoorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * created by lokizero00 on 2019-03-05
  */
+@Slf4j
+@Service
+@Transactional
 public class DoorServiceImpl implements DoorService {
 }
